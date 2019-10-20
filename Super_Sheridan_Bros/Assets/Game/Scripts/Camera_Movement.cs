042378f8dc11f5311594c51d00871e550324a63f
shreyasbabu <shreyasbabu@users.noreply.github.com>
2019-10-20 13:45:45 -0400

    Walking and Camera Follow

---
+++ Super_Sheridan_Bros/Assets/Game/Scripts/Camera_Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Movement : MonoBehaviour
+{
+    [SerializeField] float distanceAway = 5.0f;
+    [SerializeField] float distanceUp = 2.0f;
+    [SerializeField] float smooth = 2.5f;
+
+    private Transform follow;
+    private Vector3 targetPos;
+
+    private void Awake()
+    {
+        follow = GameObject.Find("follow").transform;
+    }
+
+    private void LateUpdate()
+    {
+        cameraMovement();
+    }
+
+    void cameraMovement()
+    {
+        targetPos = (follow.position + follow.up * distanceUp - follow.forward * distanceAway);
+
+        Debug.DrawRay(follow.position, Vector3.up, Color.red);
+        Debug.DrawRay(follow.position, (-1f * follow.forward * distanceAway), Color.blue);
+        Debug.DrawLine(follow.position, targetPos, Color.green);
+
+        this.transform.position = Vector3.Lerp( this.transform.position, targetPos ,Time.deltaTime * smooth);
+
+        transform.LookAt(follow);
+    }
+
+}
---
+++ Super_Sheridan_Bros/Assets/Game/Scripts/Character_Movement_Logic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Movement_Logic : MonoBehaviour
+{
+
+    [SerializeField] Animator charAnimator;
+    [SerializeField] float dampingTime = 0.25f;
+
+    private float speed = 0.0f;
+    private float hori = 0.0f;
+    private float vert = 0.0f;
+
+    private void Awake()
+    {
+        charAnimator = GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+        characterMovement();
+    }
+
+
+    void characterMovement()
+    {
+
+        hori = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+
+        speed = new Vector2(hori, vert).sqrMagnitude;
+
+        if (charAnimator != null)
+        {
+            charAnimator.SetFloat("HDirection", hori, dampingTime, Time.deltaTime);
+            charAnimator.SetFloat("VDirection", vert, dampingTime, Time.deltaTime);
+            charAnimator.SetFloat("Speed", speed);
+        }
+    }
+}
